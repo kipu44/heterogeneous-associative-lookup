dd78f040dd9545a193a836c38d0e786856ea58ab
rafal <kipu44@wp.pl>
2019-10-13 23:59:33 +0200

    Simple lookup.

--- main.cpp
+++ main.cpp
@@ -35,21 +35,25 @@ void find(const M & map, const T & t)
 
 int main()
 {
+    Raii r1(11);
+
     const Foo f1;
     const Foo f2;
     const Foo f3;
     {
-        const std::map<Bar, int, std::less<Bar>> map =
+        const std::map<Bar, int> map =
         {
-            {Bar(f1), 1},
-            {Bar(f2), 2},
-            {Bar(f3), 3},
+            {f1, 1},
+            {f2, 2},
+            {f3, 3},
         };
 
-        { Raii r(1); find(map, Bar(f2)); }
-        { Raii r(2); find(map, f2); }
-        { Raii r(3); find(map, Bar(Foo())); }
-        { Raii r(4); find(map, Foo()); }
+        { Raii r2(22); }
+
+        { Raii r(1); find(map, Bar(Foo())); }
+        { Raii r(2); find(map, Foo()); }
+        { Raii r(3); find(map, Bar(f2)); }
+        { Raii r(4); find(map, f2); }
     }
     return 0;
 }
